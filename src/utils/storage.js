e28a9afd547cf26b8eb5a6a2c69fc9f3fe29f2a2
渣臻 <qz519189636@qq.com>
2020-07-25 18:34:19 +0800

    Create storage.js

---
+++ src/utils/storage.js
@@ -0,0 +1,23 @@
+const localStorage = window.localStorage
+
+export default {
+  setItem(key,value){
+    value = JSON.stringify(value)
+    localStorage.setItem(key,value)
+  },
+  getItem(key,defaultValue){
+    let  value  = localStorage.getItem(key)
+    try{
+      value = JSON.parse(value);
+    }catch(e){ 
+      // TODO:捕获错误
+    }
+    return value || defaultValue
+  },
+  removeItem(key){
+    localStorage.removeItem(key)
+  },
+  clear(){
+    localStorage.clear() 
+  }
+}
